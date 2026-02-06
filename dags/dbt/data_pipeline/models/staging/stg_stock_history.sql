e9ea825691a27355ba9d1af760c413be818a2ac7
stinky <peiyuanlee@g.ucla.edu>
2026-02-05 21:46:58 -0800

    fixed fact table logic

--- dags/dbt/data_pipeline/models/staging/stg_stock_history.sql
+++ dags/dbt/data_pipeline/models/staging/stg_stock_history.sql
@@ -1,2 +1,24 @@
-SELECT * FROM
-{{ source('stock_data', 'stock_history')}} 
+with raw_data as (
+  select
+    symbol,
+    date,
+    open,
+    high,
+    low,
+    close,
+    volume,
+    adjclose
+  from {{ source('stock_data', 'stock_history')}} 
+)
+
+select
+  symbol,
+  date,
+  max(open)     as open,
+  max(high)     as high,
+  max(low)      as low,
+  max(close)    as close,
+  max(adjclose) as adjclose,
+  max(volume)   as volume
+from raw_data
+group by symbol,date
